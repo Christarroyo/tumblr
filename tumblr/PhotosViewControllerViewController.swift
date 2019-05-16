97f53371eeea734c264276eee0cfb18d1a6695de
Christopher Arroyo <carroyo11@lib5468-04.local>
2019-05-16 12:15:05 -0700

    1-2

---
+++ tumblr/PhotosViewControllerViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PhotosViewControllerViewController.swift
+//  tumblr
+//
+//  Created by Christopher Arroyo on 5/16/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewControllerViewController: UIViewController {
+
+    var posts: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
